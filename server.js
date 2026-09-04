c6246d1ad4a25f1af61f821ea7119c8bc0d0ecd8
radhagboss-code <radhagboss@gmail.com>
2026-09-04 23:50:10 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const { Pool } = require("pg");
 
 const app = express();
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
 const PORT = process.env.PORT || 3000;
